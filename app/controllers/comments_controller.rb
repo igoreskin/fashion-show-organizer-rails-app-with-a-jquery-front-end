ce133e2c3db57ee2beaa354331af851e2e1a6823
Igor Eskin <github email address>
2018-01-18 22:10:34 -0500

    Delete functionality added to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,6 +35,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to designs_path
+  end
+
   private
 
   def comment_params
